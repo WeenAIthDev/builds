e2cd66f894d1edfd63b4a5ae7e4491fc6e0b81f6
Technodox10 <rafeliaedwigefernandes97@gmail.com>
2026-06-01 13:55:26 +0530

    Update for learning processing layerin API flow

--- SmartFlowAPI/main.py
+++ SmartFlowAPI/main.py
@@ -1,12 +1,32 @@
 from fastapi import FastAPI #get the tools
 from pydantic import BaseModel
 app = FastAPI() #creates backend app
-@app.get("/health") #creates health endpoint and checks if the endpoint exists in backend system if it is then root() gets executed  
-@app.get("/add/{operator}/{operator2}")
+#@app.get("/health") #creates health endpoint and checks if the endpoint exists in backend system if it is then root() gets executed  
+#@app.get("/add/{operator}/{operator2}")
+def processing_stuff(message):
+    msg = str(message.message)
+    up = msg.upper()    
+    words = msg.split(" ")
+    wcount = len(words)
+    return msg,up,wcount #tuple
 class Item(BaseModel):
-    name: str
-    price: float
-    description: str | None = None
+   name: str
+   price: float
+   description: str | None = None
+
+class Message(BaseModel):
+    message: str
+
+@app.post("/process-text")
+async def message_process(message:Message):
+    ps = processing_stuff(message)
+    processed_msg = {
+        "original":ps[0],
+        "uppercase":ps[1],
+        "word_count":ps[2]
+
+    }
+    return processed_msg
 
 @app.post("/items/")
 async def items_add(item: Item):
