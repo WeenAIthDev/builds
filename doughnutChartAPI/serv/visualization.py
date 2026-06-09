143a8da637baa31a3b8666428596675b10fbbc30
Technodox10 <rafeliaedwigefernandes97@gmail.com>
2026-06-09 14:01:52 +0530

    Update 10 add secure https:// to link

--- doughnutChartAPI/serv/visualization.py
+++ doughnutChartAPI/serv/visualization.py
@@ -25,7 +25,8 @@ async def analyze_sales(data:Data):
 cloudinary.config(
     cloud_name=config.CLOUDINARY_CLOUD_NAME,
     api_key=config.CLOUDINARY_API_KEY,
-    api_secret=config.CLOUDINARY_API_SECRET
+    api_secret=config.CLOUDINARY_API_SECRET,
+    secure=True
 ) #to use cloudinary by letting it recongnise which account 
     
 def processing_sales(dt):
